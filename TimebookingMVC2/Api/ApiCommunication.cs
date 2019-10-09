c93ef8e45af31c5923fc031b6a3829cf92278831
Can Löfstrand <Can.Lofstrand@yaelev.se>
2019-10-09 19:38:02 +0200

    Add booking view

--- TimebookingMVC2/Api/ApiCommunication.cs
+++ TimebookingMVC2/Api/ApiCommunication.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
+using TimebookingMVC2.Api.Models;
 
 namespace TimebookingMVC2.Api
 {
